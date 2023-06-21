cd51f68c71f27b87445b0f3c8fcfe994cba5ebb4
hassan5544 <hmh.lol55@gmail.com>
2023-06-22 00:53:57 +0300

    commit y hassan upload regex solutions

--- RegexTasks/Program.cs
+++ RegexTasks/Program.cs
@@ -1,1 +1,85 @@
-﻿
+﻿using System;
+using System.Text.RegularExpressions; 
+namespace RegexTasks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1 Test
+            string HexCode = "#CD5C5C";
+            Console.WriteLine("Hex Code: " + HexCode + Services.CheckHex(HexCode));
+            HexCode = "#f08080";
+            Console.WriteLine("\nHex Code: " + HexCode + Services.CheckHex(HexCode));
+            HexCode = "#E9967A";
+            Console.WriteLine("\nHex Code: " + HexCode + Services.CheckHex(HexCode));
+            HexCode = "#EFFA07A";
+            Console.WriteLine("\nHex Code: " + HexCode + Services.CheckHex(HexCode));
+
+
+            //Q2 test 
+
+            string data = "CPP Exercises.";
+            Console.WriteLine("text :" + data);
+            Console.WriteLine("avrageLength " + Services.CalculateAvg(data));
+            data = "C# syntax is highly expressive, yet it is also simple and easy to learn.";
+            Console.WriteLine("text :" + data);
+            Console.WriteLine("avrageLength " + Services.CalculateAvg(data));
+            data = "C# is an elegant and type-safe object-oriented language";
+            Console.WriteLine("Text :" + data);
+            Console.WriteLine("avrageLength " + Services.CalculateAvg(data));
+
+            // Q3 Test
+
+            string text = "Text: amamd";
+            Console.WriteLine(": " + text);
+            Console.WriteLine(Services.Palindrome(text));
+            text = "pamamd";
+            Console.WriteLine("\nText: " + text);
+            Console.WriteLine(Services.Palindrome(text));
+            text = "ferre";
+            Console.WriteLine("\nText: " + text);
+            Console.WriteLine(Services.Palindrome(text));
+
+            // Q4 test
+
+            string Password = "Suuu$21g@";
+            Console.WriteLine("\nOriginal string: " + text + " is a valid passward ? " + Services.PasswordCheck(Password) + "\n");
+            Password = "W#1g@";
+            Console.WriteLine("\nOriginal string: " + text + " is a valid passward ? " + Services.PasswordCheck(Password) + "\n");
+            Password = "a&&g@";
+            Console.WriteLine("\nOriginal string: " + text + " is a valid passward ? " + Services.PasswordCheck(Password) + "\n");
+            Password = "sdsd723#$Amid";
+            Console.WriteLine("\nOriginal string: " + text + " is a valid passward ? " + Services.PasswordCheck(Password) + "\n");
+            Password = "sdsd723#$Amidkiouy";
+            Console.WriteLine("\nOriginal string: " + text + " is a valid passward ? " + Services.PasswordCheck(Password) + "\n");
+
+
+            // Q8 test
+
+            Console.WriteLine("Enter the Text");
+            string? Entry = Console.ReadLine();
+            if (text == null || text != "")
+            {
+                Console.WriteLine("Output Text :" + Services.RemoveSpecialCharacters(Entry));
+            }
+            else
+            {
+                Console.WriteLine("UnSupported format of Text");
+
+            }
+
+
+            // Q9 
+            Console.WriteLine("Enter the Search word:");
+            string? word = Console.ReadLine();
+
+            Console.WriteLine("Enter the Text for Search :");
+            string? searchtext = Console.ReadLine();
+            Console.WriteLine("The Word " + Services.SeachForWord(searchtext, word));
+
+        }
+
+
+    }
+}
---
+++ RegexTasks/Services.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexTasks
+{
+    public class Services
+    {
+        // Q1 
+        public static bool CheckHex(string HexCode)
+        {
+            return Regex.IsMatch(HexCode, @"[#][0-9A-Fa-f]{6}\b"); // Characters has to be length of six and contains number 1 to 9 and A to F
+        }
+
+        public static double CalculateAvg(string data)
+        {
+            string new_data = Regex.Replace(data, "[^A-Za-z ]", "");
+            double average_len = new_data.Split(' ').Select(x => x.Length).Average();
+            return Math.Round(average_len, 2);
+        }
+
+        public static bool Palindrome(string text)
+        {
+            bool result = Regex.Replace(string.Concat(text.OrderBy(x => x)), @"([a-z])\1{1}", string.Empty).Length <= 1;
+            return result;
+        }
+
+        public static bool PasswordCheck(string text)
+        {
+            bool result = text.Length >= 7 && text.Length <= 16
+            && Regex.IsMatch(text, "[A-Z]")
+            && Regex.IsMatch(text, "[a-z]")
+            && Regex.IsMatch(text, @"\d")
+            && Regex.IsMatch(text, @"[!-/:-@\[-_{-~]")
+            && !Regex.IsMatch(text, @"[^\dA-Za-z!-/:-@\[-_{-~]");
+            return result;
+        }
+
+        public static string RemoveSpecialCharacters(string text)
+        {
+            return Regex.Replace(text, "[^0-9A-Za-z _-]", "");
+        }
+
+        public static string SeachForWord(string? text, string? word)
+        {
+            string regex = Regex.Escape(word);
+            if (Regex.IsMatch(text, regex))
+            {
+                return word + " Exists";
+            }
+            else
+            {
+                return word + " Dont Exists";
+            }
+
+        }
+    }
+}
+}
